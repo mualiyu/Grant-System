9c8d9ed06549c9851d57634ab685c5ca21bb9a5f
mualiyu <mualiyuoox@gmail.com>
2023-05-12 19:31:54 +0100

    Added Authentication factors

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -124,14 +124,14 @@ class ApplicantController extends Controller
                     'title' => 'Your password reset',
                     'body' => 'Use Username: ' . $user->username . ' & Password: ' . $pass,
                 ];
-            return "Your username is: ".$request->username." & password is: ".$pass;
-            Mail::to($user->email)->send(new AcceptApplicantMail($mailData));
-
+                
             $update = Applicant::where('id', '=', $applicant[0]->id)->update([
                 'password' => Hash::make($password),
             ]);
-
+                
             if ($update) {
+                return "Your username is: ".$request->username." & password is: ".$pass;
+                Mail::to($user->email)->send(new AcceptApplicantMail($mailData));
                 return response()->json([
                     'status' => true,
                     'message' => "An email is sent to your mail. Thank you!"
