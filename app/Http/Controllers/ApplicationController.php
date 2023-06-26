e2c3bb941938080eb5f548960777cf1799c9626d
Muktar Usman <mualiyuoox@gmail.com>
2023-06-26 23:09:41 +0100

    Added Tabs Progress status

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1069,7 +1069,7 @@ class ApplicationController extends Controller
                     $data['technical_requirement']['msg'] .= "";
                 }else {
                     $s_as = 0;
-                    $data['technical_requirement']['msg'] .= " and You need to add atleast one employer";
+                    $data['technical_requirement']['msg'] .= ", and You need to add atleast one employer";
                 }
 
                 $s_apr = 0;
@@ -1078,7 +1078,7 @@ class ApplicationController extends Controller
                     $data['technical_requirement']['msg'] .= "";
                 }else {
                     $s_apr = 0;
-                    $data['technical_requirement']['msg'] .= " You need to add atleast one project";
+                    $data['technical_requirement']['msg'] .= ", You need to add atleast one project";
                 }
 
                 // checking if its completed
